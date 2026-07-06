52b0e39e687dd4186ba59a5db9c51e8cc1a26ab0
Jose Angel <166237244+Thejos11@users.noreply.github.com>
2026-07-06 18:11:05 -0500

    Delete proyecto java directory

--- proyecto java/app.js
+++
@@ -1,7 +0,0 @@
-import "./src/components/app-root.js";
-
-const mount = document.getElementById('app');
-if (!mount.querySelector('app-root')) {
-  const root = document.createElement('app-root');
-  mount.appendChild(root);
-}
--- proyecto java/src/utils/production-utils.js
+++
@@ -1,5 +0,0 @@
-export function getNextProcessCode(processes = []) {
-  const codes = processes.map((process) => Number(process.code || 0));
-  const maxCode = codes.length ? Math.max(...codes) : 0;
-  return maxCode + 1;
-}
